885d35f7ca031c8ddd6444f398f2f74a466b003b
Jms_Rea <125179253+Zesty-ZDev@users.noreply.github.com>
2023-07-08 11:23:14 +1200

    V-0.1.1

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+name 'AGI Link'
+description 'Smart integration with AGI for realtime monitoring and secruity'
+author 'AGI-DEV'
+version 'v0.1.1'
+
+server_scripts {
+	"server.lua",
+	"config.lua"
+}
