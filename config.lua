281adf6b727ab05e8d565f5adcacb9fe4957db05
Jms_Rea <125179253+Zesty-ZDev@users.noreply.github.com>
2023-09-20 22:46:43 +1000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,3 @@
 --If you don't have a LinkKey use the /linkkey command in your community discord
 Config = {}
-Config.linkkey = 'OXSHEIENSOFNEOSD' -- Paste your private LinkKey inbetween the quotes
+Config.linkkey = '' -- Paste your private LinkKey inbetween the quotes
