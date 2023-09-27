6d81a47209fd42ec21371a1ec9ea492d7f7a3cce
Jms_Rea <125179253+Zesty-ZDev@users.noreply.github.com>
2023-09-27 13:46:39 +1000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
---If you don't have a LinKey use the /linkey command in your community discord 
+--If you don't have a LinKey use the /config command in your community discord 
 Config = {}
 Config.linkey = '123456' -- Paste your private LinKey inbetween the quotes
 ---Version 1.0.2
